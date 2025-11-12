cc6321d524a8fc500b874f7de7855f1da465ecaf
ruslan <ruslanone57@gmail.com>
2025-11-12 20:30:16 +0700

    Fix: parses removed

--- include/parser.h
+++
@@ -1,7 +0,0 @@
-#include "../lib/picohttpparser/picohttpparser.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-
-
-void url_callback();
--- src/parser.c
+++
@@ -1,9 +0,0 @@
-#include "parser.h"
-
-void url_callback(struct http_parser* parser, const char* at, size_t length) {
-    return;
-}
-
-
-
-
